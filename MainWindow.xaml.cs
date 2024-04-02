965d9bb26ae5ec0c43caa0cd9659a57ccc66a0e5
TON618OFF <rusblackgod@gmail.com>
2024-04-02 14:57:11 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice3EntitiesWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BookSystemPractice3Entities books = new BookSystemPractice3Entities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg_BD_books.ItemsSource = books.Books.ToList();
+        }
+    }
+}
